519f5e7a1017284e7b97b4d758f67f13ecf1c49f
Mahmoud Yassin <mahmoudyassin.dev@gmail.com>
2025-10-05 23:22:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 // ⛔️ wrong: import react from "@vitejs/plugin-react";
 // ✅ right:
 import react from "@vitejs/plugin-react-swc";
-
+base: process.env.VITE_PUBLIC_BASE || "/flood-lens-nasa/",
 export default defineConfig({
   plugins: [react()],
   // make assets resolve correctly on GitHub Pages
