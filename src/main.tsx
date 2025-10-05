06ec29c7549e63c1cbdebeffaba7ba6e5476a3ce
Mahmoud Yassin <mahmoudyassin.dev@gmail.com>
2025-10-05 22:22:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
+<BrowserRouter basename={import.meta.env.BASE_URL}>
+  <App />
+</BrowserRouter>
 createRoot(document.getElementById("root")!).render(<App />);
